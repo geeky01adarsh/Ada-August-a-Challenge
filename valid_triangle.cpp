4189b7df801c141dbc849f58e21e3f1d51bd52f9
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-08-11 17:57:20 +0530

    solution of 8th Aug ques FLOW13

---
+++ valid_triangle.cpp
@@ -0,0 +1,17 @@
+// Question Link- https://www.codechef.com/CCADAAUG/problems/FLOW013
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if((a+b+c)==180) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
