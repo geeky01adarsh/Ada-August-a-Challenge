42eb1985fe004d76fe5087999ed87b79872323a2
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-08-11 18:07:56 +0530

    solution of 10th Aug ques FSQRT

---
+++ finding_square_roots.cpp
@@ -0,0 +1,17 @@
+// Question Link -- https://www.codechef.com/CCADAAUG/problems/FSQRT
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int num;
+        cin>>num;
+        cout<<int(sqrt(num))<<endl;
+    }
+    
+}
